40e7d8ed7848a08fb4cf3287a2aa5961d12fb0a6
unknown <you@example.com>
2024-05-14 05:01:06 +0400

    added 2 commit

---
+++ MultiShop/DataAccesLayer/MultiContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Models;
+
+namespace MultiShop.DataAccesLayer
+{
+    public class MultiContext : DbContext
+    {
+        public MultiContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=DESKTOP-8QLA9CR\SQLEXPRESS;Database=MultiShop;Trusted_Connection=True;TrustServerCertificate=True");
+            base.OnConfiguring(options);
+        }
+    }
+}
